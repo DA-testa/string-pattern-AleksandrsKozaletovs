4138b3c9a6b5f52104b7084f48f8d0c2331ab7bd
AleksandrsKozaletovs <125273328+AleksandrsKozaletovs@users.noreply.github.com>
2023-04-16 23:53:50 +0300

    Update hash_substring.py
    check result #1

--- hash_substring.py
+++ hash_substring.py
@@ -1,13 +1,15 @@
 # python3
+B = 26
+Q = 256
 
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    actionChoose = input()
+      actionChoose = input()
     if  "i" in actionChoose.lower():        
     # input from keyboard
-        n = int(input())
-        data = list(map(int, input().split()))
+        patternInput = input()
+        textData = input()
 
     elif "f" in actionChoose.lower():
         openFilename = input()
@@ -16,10 +18,10 @@ def read_input():
         else:
             with open("./tests/" + openFilename, mode = "r") as f:
                 # input from keyboard
-                n = int(f.readline())
-                data = list(map(int, f.readline().split()))
-          else:
-            return
+                patternInput = f.readline()
+                textData = f.readline()
+    else:
+        return
     
     # after input type choice
     # read two lines 
@@ -29,17 +31,34 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return (patternInput.rstrip(), textData.rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
+def get_hash(pattern)-> int:
+    global B, Q
+    patternLength = len(pattern)
+    hashResult = 0
+    for i in range(patternLength):
+        hashResult = (B * hashResult + ord(pattern[i])) % Q
+    return hashResult
+
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
+    result = []
+    patternLength = len(pattern)    
+    paternHashResult = get_hash(pattern)
+    textDataLength = len(text)
+    
+    for i in range(0, textDataLength - patternLength + 1):
+        textDataHashResult = get_hash(text[i: i + patternLength])
+        if paternHashResult == textDataHashResult:
+            if pattern == text[i: i + patternLength]:
+                result.append(i)
     # and return an iterable variable
-    return [0]
+    return result
 
 
 # this part launches the functions
