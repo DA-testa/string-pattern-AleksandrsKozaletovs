79090bc558423440a25a2e6055d253e927451abd
AleksandrsKozaletovs <125273328+AleksandrsKozaletovs@users.noreply.github.com>
2023-04-16 23:18:11 +0300

    Update hash_substring.py
    adding input code

--- hash_substring.py
+++ hash_substring.py
@@ -3,7 +3,23 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
+    actionChoose = input()
+    if  "i" in actionChoose.lower():        
+    # input from keyboard
+        n = int(input())
+        data = list(map(int, input().split()))
+
+    elif "f" in actionChoose.lower():
+        openFilename = input()
+        if "a" in openFilename.lower():
+            return
+        else:
+            with open("./tests/" + openFilename, mode = "r") as f:
+                # input from keyboard
+                n = int(f.readline())
+                data = list(map(int, f.readline().split()))
+          else:
+            return
     
     # after input type choice
     # read two lines 
